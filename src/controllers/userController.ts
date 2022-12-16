9ac01357804dae7e9ba45ada11027f2e9b84bf93
dunghenry <trandungksnb6930082001>
2022-12-16 10:48:02 +0700

    changed code

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,8 +2,13 @@ import { Response, Request } from 'express';
 import User from '../models/User';
 class userController {
     static async getUsers(req: Request, res: Response) {
-        const users = await User.find();
-        return res.status(200).json(users);
+        try {
+            const users = await User.find();
+            return res.status(200).json(users);
+        } catch (err) {
+            // console.log(err);
+            return res.status(500).json(err);
+        }
     }
 }
 
